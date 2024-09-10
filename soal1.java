737bf028a945702046b2a9f4431cb1154cb30dd0
Asafati Gea <2382006@unai.edu>
2024-09-10 10:19:59 +0700

    Handson2

---
+++ soal1.java
@@ -0,0 +1,11 @@
+public class soal1 {
+
+    /**
+     * buatlah sebuah program yang mengambil 2 input number dari user
+     * Program tersebut akan print out nilai yang paling besar
+     *
+     */
+    public static void main(String[] args) {
+
+    }
+}
